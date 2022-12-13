b477b2d4c39821fd53ce00906d71358314908c0e
Agustin <agustinvila@outlook.com>
2022-12-12 21:27:02 -0300

    subida

---
+++ src/constants/data/categories.js
@@ -0,0 +1,32 @@
+export const CATEGORIES = [
+  {
+    id: 1,
+    title: 'Harinas Puras',
+    color: '#3F8EFC',
+  },
+  {
+    id: 2,
+    title: 'Harinas Integrales',
+    color: '#3F8EFC',
+  },
+  {
+    id: 3,
+    title: 'Harinas de Trigo',
+    color: '#3F8EFC',
+  },
+  {
+    id: 4,
+    title: 'Harinas de Maíz',
+    color: '#3F8EFC',
+  },
+  {
+    id: 5,
+    title: 'Harinas de Centeno',
+    color: '#3F8EFC',
+  },
+  {
+    id: 6,
+    title: 'Harinas de Cebada',
+    color: '#3F8EFC',
+  },
+];
---
+++ src/constants/themes/colors.js
@@ -0,0 +1,14 @@
+export const COLORS = {
+  primary: '#3F8EFC',
+  primaryDark: '#87BFFF',
+  secondary: '#87BFFF',
+  secondaryDark: '#87BFFF',
+  backgroundLight: '#3F8EFC',
+  backgroundDark: '#87BFFF',
+  background: '#ADD7F6',
+  text: '#000000',
+  textLight: '#212121',
+  white: '#FFFFFF',
+  black: '#000000',
+  gray: '#ADD7F6',
+};
